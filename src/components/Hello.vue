c9153f10da042bf25ed6d82a847be3ad8534ea0b
joss <sujiaxu0202@gmail.com>
2022-03-10 13:09:23 +0800

    修改txt為中文

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <div id="flutter-msg">Message from Flutter: {{msg}}</div><br>
-    <input id="inputBox" v-model="inputMsg" placeholder="output to flutter">
-    <p id="inputMsg">Input Message is: {{inputMsg}}</p>
-    <button>click to open Flutter page</button>
+    <div id="flutter-msg">從Flutter傳入的值: {{msg}}</div>
+    <div>
+      請輸入：<input id="inputBox" v-model="inputMsg" placeholder="output to flutter">
+    </div>
+    <p id="inputMsg">欲傳給Flutter的值: {{inputMsg}}</p>
+    <button>點擊傳值給Flutter</button>
   </div>
 </template>
 <script>
@@ -26,5 +28,7 @@ export default {
 }
 </script>
 <style scoped>
-
+#flutter-msg {
+  margin-bottom: 40px;
+}
 </style>
