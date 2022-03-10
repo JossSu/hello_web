48b1df4b8fc2a537ba719ca6692c31e1fdd285d6
joss <sujiaxu0202@gmail.com>
2022-03-10 10:52:49 +0800

    create vue project

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <div>Hello!!!</div>
+    <button>click</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Hello',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
