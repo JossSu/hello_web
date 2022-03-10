7eac37dc55f9592d4c2aa86f5f0c759930253eb2
joss <sujiaxu0202@gmail.com>
2022-03-10 15:11:32 +0800

    install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <div id="app">
-    <Hello/>
+  <div>
+    <div id="flutter-msg">從Flutter傳入的值: {{msg}}</div>
+    <div>
+      請輸入：<input id="inputBox" v-model="inputMsg" placeholder="output to flutter">
+    </div>
+    <div>
+      <p>欲傳給Flutter的值:<span id="inputMsg">{{inputMsg}}</span></p>
+    </div>
+    <button @click="invokeNative">點擊傳值給Flutter</button><br>
+    <!--    <router-link to="http://youtube.com.tw">-->
+    <button style="margin-top: 10px" @click="invokeNativeJump">點擊傳值給Flutter並且跳到指定頁面</button>
+    <!--    </router-link>-->
   </div>
 </template>
-
 <script>
-import Hello from './components/Hello.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Hello
+  name: 'Hello',
+  data(){
+    return {
+      msg: 'Hello!',
+      inputMsg: ''
+    }
+  },
+  mounted() {
+    window.fromFlutter = this.fromFlutter
+  },
+  methods: {
+    fromFlutter(location) {
+      document.getElementById("flutter-msg").innerHTML = location;
+    },
+    invokeNative() {
+      window['Toast'].postMessage(this.inputMsg);
+    },
+    invokeNativeJump() {
+      this.$router.push('flutterweb://result?value=cool')
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#flutter-msg {
+  margin-bottom: 40px;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hello from '@/components/Hello.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Hello
+  }
+}
+</script>
