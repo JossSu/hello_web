872483fe6787ee35651436bb4e7e67a8926a8062
joss <sujiaxu0202@gmail.com>
2022-03-14 12:23:32 +0800

    show query on the page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="landing">
-    Landing...
+    Hi {{input}}, <br>How's your day?
   </div>
 </template>
 <script>
@@ -8,11 +8,19 @@ export default {
   name: 'Landing',
   data() {
     return {
-
+      input: this.$route.query.input
     }
-  }
+  },
+  // mounted() {
+  //   console.log('route', this.$route.query.input)
+  // }
 }
 </script>
 <style scoped>
-
+.landing {
+  display: flex;
+  justify-content: center;
+  padding-top: 20vh;
+  font-size: 30px;
+}
 </style>
